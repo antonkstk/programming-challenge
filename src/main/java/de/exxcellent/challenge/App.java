f30312cca101905329a227cba5bd03e55b752c4f
Anton_Kostiuchenko <anton.kostiuchenko@gmail.com>
2017-09-22 01:37:30 +0200

    adding some comments to the code

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -12,6 +12,7 @@ public final class App {
 
         CSVReader reader = new CSVReader();
 
+        // parse csv data files by specifying the needed columns
         String dayWithSmallestTempSpread = reader.readCSV("weather.csv", 1, 2);     // Your day analysis function call …
         String teamWithSmallesGoalSpread = reader.readCSV("football.csv", 5, 6); // Your goal analysis function call …
 
--- src/main/java/de/exxcellent/challenge/CSVReader.java
+++ src/main/java/de/exxcellent/challenge/CSVReader.java
@@ -8,15 +8,25 @@ import java.io.*;
 public class CSVReader {
 
    private String result;
-   private int smallestSpread = 100;
+   private int smallestSpread;
 
+   /**
+    * this method contains the general logic of
+    * reading a text file
+    * @param filePath to a file in the resources folder
+    * @param column_1 first column we have to work with
+    * @param column_2 second column we have to work with
+    * @return either Day or Football team's name
+    */
    public String readCSV(String filePath, int column_1, int column_2) {
 
       try {
          InputStream stream = getClass().getResourceAsStream(filePath);
          BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
          String line;
-
+         // reset the values if used before
+         smallestSpread = 100;
+         result = "";
          // skip the first line
          reader.readLine();
 
@@ -32,10 +42,19 @@ public class CSVReader {
       return result;
    }
 
+   /**
+    * calculating the diff between two values of the same record
+    * @param dataLine parsed line, separated by ,
+    * @return diff
+    */
    private int calculateDiff(String[] dataLine, int column_1, int column_2) {
       return Math.abs(Integer.parseInt(dataLine[column_1]) - Integer.parseInt(dataLine[column_2]));
    }
 
+   /**
+    * process the parsed data according to the given task (weather challenge)
+    * @param line one line from buffer
+    */
    private void processData(String line, int column_1, int column_2) {
       String separator = ",";
       String[] record = line.split(separator);
